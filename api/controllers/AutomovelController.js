cbce66712cd72e82e961d6656c2f167581af32e1
Gabriel Ramos <gabramos1@publicisgroupe.net>
2018-07-23 14:23:08 -0300

    atualizando userss

--- api/controllers/AutomovelController.js
+++ api/controllers/AutomovelController.js
@@ -8,7 +8,7 @@
 module.exports = {
 	list:function(req,res){
 	   Automovel.query('SELECT * from c9.Automovel;', [], function(err, automoveis) {
-	        if(err) { 
+	        if(err) {
 	            res.send(500, {error: "Database error"});
 	        }
 	        res.view('automovel/list', {automoveis: automoveis});
@@ -18,9 +18,8 @@ module.exports = {
 	    res.view('automovel/add');
     },
     create: function(req,res){
-    	console.log(req.body);
-		
-		Automovel.query(`INSERT INTO c9.Automovel (Placa, Marca, Modelo, Ano, Cambio, Combustivel, Direcao, PotenciaMotor, Tipo, Portas, Cor, Chassi, Disponibilidade, AirBag, ArCondicionado, Alarme, TravaEletrica, Som, VidroEletrico, Blindado, SensorRe, CameraRe, Titulo, Descricao, FotoVeiculoFrente, FotoVeiculoInterior, FotoVeiculoLateral, FotoDocumento, PossuiSeguro, Status, Cidade, Estado, Avaliacao, NumeroAvaliacoes, PrecoDiaria, CPF) VALUES("${req.body.Placa}", "${req.body.Marca}", "${req.body.Modelo}", "${req.body.Ano}", "${req.body.Cambio}", "${req.body.Combustivel}", "${req.body.Direcao}", "${req.body.Potencia}", "${req.body.tipoVeiculo}", "${req.body.numPortas}", "${req.body.corVeiculo}", "${req.body.Chassi}", "D", "${req.body.AirBag}", "${req.body.ArCondicionado}", "${req.body.Alarme}", "${req.body.travaEletrica}", "${req.body.Som}", "${req.body.vidroEletrico}", "${req.body.Blindado}", "${req.body.sensorRe}",  "${req.body.cameraRe}", "${req.body.Titulo}", "${req.body.Descricao}", "images/automovel/fiatUno.jpg", "images/automovel/fiatUno.jpg", "images/automovel/fiatUno.jpg", "images/automovel/fiatUno.jpg", "1", "Disponivel", "${req.body.cidadesEstado}", "${req.body.estadosBrasil}", "5", "0}", "${req.body.precoDiaria}", "02840193719");`, [], function(err, usuarios){
+
+		  Automovel.query(`INSERT INTO c9.Automovel (Placa, Marca, Modelo, Ano, Cambio, Combustivel, Direcao, PotenciaMotor, Tipo, Portas, Cor, Chassi, Disponibilidade, AirBag, ArCondicionado, Alarme, TravaEletrica, Som, VidroEletrico, Blindado, SensorRe, CameraRe, Titulo, Descricao, FotoVeiculoFrente, FotoVeiculoInterior, FotoVeiculoLateral, FotoDocumento, PossuiSeguro, Status, Cidade, Estado, Avaliacao, NumeroAvaliacoes, PrecoDiaria, CPF) VALUES("${req.body.Placa}", "${req.body.Marca}", "${req.body.Modelo}", "${req.body.Ano}", "${req.body.Cambio}", "${req.body.Combustivel}", "${req.body.Direcao}", "${req.body.Potencia}", "${req.body.tipoVeiculo}", "${req.body.numPortas}", "${req.body.corVeiculo}", "${req.body.Chassi}", "D", "${req.body.AirBag}", "${req.body.ArCondicionado}", "${req.body.Alarme}", "${req.body.travaEletrica}", "${req.body.Som}", "${req.body.vidroEletrico}", "${req.body.Blindado}", "${req.body.sensorRe}",  "${req.body.cameraRe}", "${req.body.Titulo}", "${req.body.Descricao}", "images/automovel/fiatUno.jpg", "images/automovel/fiatUno.jpg", "images/automovel/fiatUno.jpg", "images/automovel/fiatUno.jpg", "1", "Disponivel", "${req.body.cidadesEstado}", "${req.body.estadosBrasil}", "5", "0}", "${req.body.precoDiaria}", "02840193719");`, [], function(err, usuarios){
 	        if(err){
 	            res.send(500, {error: "Database error"});
 	    	}
@@ -41,15 +40,15 @@ module.exports = {
 			if(err){
 	    		res.send(500, {error: "Database error"});
 	    	}
-	    	
+
 	    	res.view('automovel/edit', {automovel: automovel[0]});
 		});
 	},
 	update: function(req,res){
 		console.log(req.body);
-		
+
 		var AirBag, ArCondicionado, Alarme, TravaEletrica, Som, VidroEletrico, Blindado, SensorRe, CameraRe;
-		
+
 		if (req.body.AirBag && req.body.AirBag == 'on') {AirBag = 1} else { AirBag = 0}
 		if (req.body.ArCondicionado && req.body.ArCondicionado == 'on') {ArCondicionado = 1} else { ArCondicionado = 0}
 		if (req.body.Alarme && req.body.Alarme == 'on') {Alarme = 1} else { Alarme = 0}
@@ -59,36 +58,36 @@ module.exports = {
 		if (req.body.Blindado && req.body.Blindado == 'on') {Blindado = 1} else { Blindado = 0}
 		if (req.body.sensorRe && req.body.sensorRe == 'on') {SensorRe = 1} else { SensorRe = 0}
 		if (req.body.cameraRe && req.body.cameraRe == 'on') {CameraRe = 1} else { CameraRe = 0}
-		
+
 		Automovel.query(`UPDATE c9.Automovel SET \
-		Marca= "${req.body.Marca}", \ 
-		Modelo= "${req.body.Modelo}", \ 
-		Ano= "${req.body.Ano}", \ 
-		Cambio= "${req.body.Cambio}", \ 
-		Combustivel= "${req.body.Combustivel}", \ 
-		Direcao= "${req.body.Direcao}", \ 
-		PotenciaMotor= "${req.body.Potencia}", \ 
-		Tipo = "${req.body.tipoVeiculo }", \ 
-		Portas= "${req.body.numPortas}", \ 
-		Cor= "${req.body.corVeiculo}", \ 
-		Chassi= "${req.body.Chassi}", \ 
-		AirBag= "${AirBag}", \ 
-		ArCondicionado= "${ArCondicionado}", \ 
-		Alarme= "${Alarme}", \ 
-		TravaEletrica= "${TravaEletrica}", \ 
-		Som= "${Som}", \ 
-		VidroEletrico= "${VidroEletrico}", \ 
-		Blindado= "${Blindado}", \ 
-		SensorRe= "${SensorRe}", \ 
-		CameraRe= "${CameraRe}", \ 
-		Titulo= "${req.body.Titulo}", \ 
-		Descricao= "${req.body.Descricao}", \ 
-		FotoVeiculoFrente= "images/automovel/fiatUno.jpg", \ 
-		FotoVeiculoInterior= "images/automovel/fiatUno.jpg", \ 
-		FotoVeiculoLateral= "images/automovel/fiatUno.jpg", \ 
-		FotoDocumento= "images/automovel/fiatUno.jpg", \ 
-		Cidade= "${req.body.cidadesEstado}", \ 
-		Estado= "${req.body.estadosBrasil}", \ 
+		Marca= "${req.body.Marca}", \
+		Modelo= "${req.body.Modelo}", \
+		Ano= "${req.body.Ano}", \
+		Cambio= "${req.body.Cambio}", \
+		Combustivel= "${req.body.Combustivel}", \
+		Direcao= "${req.body.Direcao}", \
+		PotenciaMotor= "${req.body.Potencia}", \
+		Tipo = "${req.body.tipoVeiculo }", \
+		Portas= "${req.body.numPortas}", \
+		Cor= "${req.body.corVeiculo}", \
+		Chassi= "${req.body.Chassi}", \
+		AirBag= "${AirBag}", \
+		ArCondicionado= "${ArCondicionado}", \
+		Alarme= "${Alarme}", \
+		TravaEletrica= "${TravaEletrica}", \
+		Som= "${Som}", \
+		VidroEletrico= "${VidroEletrico}", \
+		Blindado= "${Blindado}", \
+		SensorRe= "${SensorRe}", \
+		CameraRe= "${CameraRe}", \
+		Titulo= "${req.body.Titulo}", \
+		Descricao= "${req.body.Descricao}", \
+		FotoVeiculoFrente= "images/automovel/fiatUno.jpg", \
+		FotoVeiculoInterior= "images/automovel/fiatUno.jpg", \
+		FotoVeiculoLateral= "images/automovel/fiatUno.jpg", \
+		FotoDocumento= "images/automovel/fiatUno.jpg", \
+		Cidade= "${req.body.cidadesEstado}", \
+		Estado= "${req.body.estadosBrasil}", \
 		PrecoDiaria= "${req.body.precoDiaria}" \
 		WHERE Placa="${req.body.Placa}";`, [], function(err, p){
 		        if(err){
@@ -104,7 +103,7 @@ module.exports = {
 	    	}
 	    	res.redirect('automovel/list')
 	    });
-	    
+
 	    return false;
 	}
 };
--- api/controllers/UsuarioController.js
+++ api/controllers/UsuarioController.js
@@ -8,7 +8,7 @@ const util = require('util');
 
 module.exports = {
   list:function(req,res){
-    Usuario.query('SELECT * from c9.Usuario;', [], function(err, usuarios) {
+    Usuario.query('SELECT * from c9.usuario;', [], function(err, usuarios) {
       if(err) {
         res.send(500, {error: "Database error"});
       }
@@ -20,17 +20,19 @@ module.exports = {
     res.view('usuario/add');
   },
   create: function(req,res){
-    res.redirect('/');
-    Usuario.query(`INSERT INTO banco.usuario (cpf, nome, nascimento, rg, email, senha, endereco, telefone, cidade, estado, sexo) VALUES( "${req.body.cpf}", "${req.body.nome}", "${req.body.nascimento}", "${req.body.rg}", "${req.body.email}", "${req.body.senha}", "${req.body.endereco}", "${req.body.telefone}", "${req.body.cidade}", "${req.body.estado}", "${req.body.sexo}");`, [], function(err, usuarios){
+    console.log(req.body);
+    Usuario.query(`INSERT INTO c9.usuario (cpf, nome, nascimento, rg, email, senha, endereco, telefone, cidade, estado, sexo) VALUES( "${req.body.cpf}", "${req.body.nome}", "${req.body.nascimento}", "${req.body.rg}", "${req.body.email}", "${req.body.senha}", "${req.body.endereco}", "${req.body.telefone}", "${req.body.cidade}", "${req.body.estado}", "${req.body.sexo}");`, [], function(err, usuarios){
       if(err){
         res.send(500, {error: "Database error"});
       }
-      else
+      else {
         console.log("cadastro criado com sucesso!");
+        res.redirect('/');
+      }
     });
   },
   about: function(req,res){
-    Usuario.query(`SELECT * from c9.Usuario WHERE CPF="${req.params.id}";`, ['u'], function(err, usuario) {
+    Usuario.query(`SELECT * from c9.usuario WHERE CPF="${req.params.id}";`, ['u'], function(err, usuario) {
       if(err){
         res.send(500, {error: "Database error"});
       }
@@ -47,12 +49,21 @@ module.exports = {
     });
   },
   update: function(req,res){
-    console.log(`${req.body.nascimento}`);
-    Usuario.query(`UPDATE banco.usuario SET nome="${req.body.nome}", nascimento="${req.body.nascimento}", rg="${req.body.rg}", email="${req.body.email}", senha="${req.body.senha}", endereco="${req.body.endereco}", telefone="${req.body.telefone}", cidade="${req.body.cidade}", estado="${req.body.estado}" WHERE cpf="${req.body.cpf}";`, [], function(err, p){
+
+    Usuario.query(`UPDATE banco.usuario SET \
+      nome="${req.body.nome}", \
+      nascimento="${req.body.nascimento}", \
+      rg="${req.body.rg}", email="${req.body.email}", \
+      senha="${req.body.senha}", \
+      endereco="${req.body.endereco}", \
+      telefone="${req.body.telefone}", \
+      cidade="${req.body.cidade}", \
+      estado="${req.body.estado}" \
+      WHERE cpf="${req.body.cpf}";`, [], function(err, p){
       if(err){
         res.send(500, {error: "Database error"});
       }
-      res.redirect('/usuario/list');
+      res.redirect('/usuario/edit');
     });
   },
   delete:function(req,res){
@@ -66,6 +77,3 @@ module.exports = {
     return false;
   }
 };
-
-//cpf="${req.body.cpf}"
-//, nascimento="${req.body.nascimento}", rg="${req.body.rg}", email="${req.body.email}", senha="${req.body.senha}", endereco="${req.body.endereco}", telefone="${req.body.telefone}", cidade="${req.body.cidade}", estado="${req.body.estado}"
