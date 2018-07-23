9aa75333e0405112bf1d30844472c2fa9a842f71
Matheus Eduardo Freitag <freitag.matheus@gmail.com>
2018-07-23 02:36:43 +0000

    Automóveis quase prontos em CRU, falta D)

--- api/controllers/AutomovelController.js
+++ api/controllers/AutomovelController.js
@@ -19,26 +19,6 @@ module.exports = {
     },
     create: function(req,res){
     	console.log(req.body);
-  //  	var AirBag = "0";
-		// var ArCondicionado = "0";
-		// var Alarme = "0";
-		// var TravaEletrica = "0";
-		// var Som = "0";
-		// var VidroEletrico = "0";
-		// var Blindado = "0";
-		// var SensorRe = "0";
-		// var CameraRe = "0";
-
-		// if (req.body.AirBag) {AirBag = "1"}
-		// if (req.body.ArCondicionado){ ArCondicionado = "1"}
-		// if (req.body.Alarme){ Alarme = "1"}
-		// if (req.body.TravaEletrica){ TravaEletrica = "1"}
-		// if (req.body.Som){ Som = "1"}
-		// if (req.body.VidroEletrico){ VidroEletrico = "1"}
-		// if (req.body.Blindado){ Blindado = "1"}
-		// if (req.body.SensorRe){ SensorRe = "1"}
-		// if (req.body.CameraRe){ CameraRe = "1"}
-		
 		
 		Automovel.query(`INSERT INTO c9.Automovel (Placa, Marca, Modelo, Ano, Cambio, Combustivel, Direcao, PotenciaMotor, Tipo, Portas, Cor, Chassi, Disponibilidade, AirBag, ArCondicionado, Alarme, TravaEletrica, Som, VidroEletrico, Blindado, SensorRe, CameraRe, Titulo, Descricao, FotoVeiculoFrente, FotoVeiculoInterior, FotoVeiculoLateral, FotoDocumento, PossuiSeguro, Status, Cidade, Estado, Avaliacao, NumeroAvaliacoes, PrecoDiaria, CPF) VALUES("${req.body.Placa}", "${req.body.Marca}", "${req.body.Modelo}", "${req.body.Ano}", "${req.body.Cambio}", "${req.body.Combustivel}", "${req.body.Direcao}", "${req.body.Potencia}", "${req.body.tipoVeiculo}", "${req.body.numPortas}", "${req.body.corVeiculo}", "${req.body.Chassi}", "D", "${req.body.AirBag}", "${req.body.ArCondicionado}", "${req.body.Alarme}", "${req.body.travaEletrica}", "${req.body.Som}", "${req.body.vidroEletrico}", "${req.body.Blindado}", "${req.body.sensorRe}",  "${req.body.cameraRe}", "${req.body.Titulo}", "${req.body.Descricao}", "images/automovel/fiatUno.jpg", "images/automovel/fiatUno.jpg", "images/automovel/fiatUno.jpg", "images/automovel/fiatUno.jpg", "1", "Disponivel", "${req.body.cidadesEstado}", "${req.body.estadosBrasil}", "5", "0}", "${req.body.precoDiaria}", "02840193719");`, [], function(err, usuarios){
 	        if(err){
@@ -66,10 +46,51 @@ module.exports = {
 		});
 	},
 	update: function(req,res){
-		var ps,d;
-		if(req.body.possuiSeguro && req.body.possuiSeguro=="on"){ps = "1";} else {ps = "0";};
-		if(req.body.disponibilidade && req.body.disponibilidade=="on"){d = "1";} else {d = "0";};
-		Automovel.query(`UPDATE c9.automovel SET modelo = "${req.body.modelo}", cor = "${req.body.cor}", chassi = "${req.body.chassi}", dataInicio = "${req.body.dataInicio}", dataFim = "${req.body.dataFim}", cidade = "${req.body.cidade}", estado = "${req.body.estado}", disponibilidade = "${d}", possuiSeguro = "${ps}", descricao = "${req.body.descricao}" WHERE placa="${req.body.placa}";`, [], function(err, p){
+		console.log(req.body);
+		
+		var AirBag, ArCondicionado, Alarme, TravaEletrica, Som, VidroEletrico, Blindado, SensorRe, CameraRe;
+		
+		if (req.body.AirBag && req.body.AirBag == 'on') {AirBag = 1} else { AirBag = 0}
+		if (req.body.ArCondicionado && req.body.ArCondicionado == 'on') {ArCondicionado = 1} else { ArCondicionado = 0}
+		if (req.body.Alarme && req.body.Alarme == 'on') {Alarme = 1} else { Alarme = 0}
+		if (req.body.travaEletrica && req.body.travaEletrica == 'on') {TravaEletrica = 1} else { TravaEletrica = 0}
+		if (req.body.Som && req.body.Som == 'on') {Som = 1} else { Som = 0}
+		if (req.body.vidroEletrico && req.body.vidroEletrico == 'on') {VidroEletrico = 1} else { VidroEletrico = 0}
+		if (req.body.Blindado && req.body.Blindado == 'on') {Blindado = 1} else { Blindado = 0}
+		if (req.body.sensorRe && req.body.sensorRe == 'on') {SensorRe = 1} else { SensorRe = 0}
+		if (req.body.cameraRe && req.body.cameraRe == 'on') {CameraRe = 1} else { CameraRe = 0}
+		
+		Automovel.query(`UPDATE c9.Automovel SET \
+		Marca= "${req.body.Marca}", \ 
+		Modelo= "${req.body.Modelo}", \ 
+		Ano= "${req.body.Ano}", \ 
+		Cambio= "${req.body.Cambio}", \ 
+		Combustivel= "${req.body.Combustivel}", \ 
+		Direcao= "${req.body.Direcao}", \ 
+		PotenciaMotor= "${req.body.Potencia}", \ 
+		Tipo = "${req.body.tipoVeiculo }", \ 
+		Portas= "${req.body.numPortas}", \ 
+		Cor= "${req.body.corVeiculo}", \ 
+		Chassi= "${req.body.Chassi}", \ 
+		AirBag= "${AirBag}", \ 
+		ArCondicionado= "${ArCondicionado}", \ 
+		Alarme= "${Alarme}", \ 
+		TravaEletrica= "${TravaEletrica}", \ 
+		Som= "${Som}", \ 
+		VidroEletrico= "${VidroEletrico}", \ 
+		Blindado= "${Blindado}", \ 
+		SensorRe= "${SensorRe}", \ 
+		CameraRe= "${CameraRe}", \ 
+		Titulo= "${req.body.Titulo}", \ 
+		Descricao= "${req.body.Descricao}", \ 
+		FotoVeiculoFrente= "images/automovel/fiatUno.jpg", \ 
+		FotoVeiculoInterior= "images/automovel/fiatUno.jpg", \ 
+		FotoVeiculoLateral= "images/automovel/fiatUno.jpg", \ 
+		FotoDocumento= "images/automovel/fiatUno.jpg", \ 
+		Cidade= "${req.body.cidadesEstado}", \ 
+		Estado= "${req.body.estadosBrasil}", \ 
+		PrecoDiaria= "${req.body.precoDiaria}" \
+		WHERE Placa="${req.body.Placa}";`, [], function(err, p){
 		        if(err){
 		            res.send(500, {error: "Database error"});
 		        }
