b365e2aa8a23798672d23722a21660e4e4d6e9e8
Matheus Eduardo Freitag <freitag.matheus@gmail.com>
2018-07-24 02:26:47 +0000

    CRU de Usuario feito

--- api/controllers/UsuarioController.js
+++ api/controllers/UsuarioController.js
@@ -20,14 +20,24 @@ module.exports = {
     res.view('usuario/add');
   },
   create: function(req,res){
-    res.redirect('/');
-    Usuario.query(`INSERT INTO banco.usuario (cpf, nome, nascimento, rg, email, senha, endereco, telefone, cidade, estado, sexo) VALUES( "${req.body.cpf}", "${req.body.nome}", "${req.body.nascimento}", "${req.body.rg}", "${req.body.email}", "${req.body.senha}", "${req.body.endereco}", "${req.body.telefone}", "${req.body.cidade}", "${req.body.estado}", "${req.body.sexo}");`, [], function(err, usuarios){
-      if(err){
-        res.send(500, {error: "Database error"});
-      }
-      else
-        console.log("cadastro criado com sucesso!");
-    });
+    console.log(req.body);
+    
+    req.file('FotoPerfil').upload({ 
+    		dirname: require('path').resolve(sails.config.appPath, 'assets/images/usuario'),
+    		saveAs: req.body.CPF + '_' + "FotoPerfil." + (req.file('FotoPerfil')._files[0].stream.filename).substr(req.file('FotoPerfil')._files[0].stream.filename.length - 3)
+    	}, function (err, uploadedFiles) {
+		      console.log(uploadedFiles);
+		  
+			      var lista = uploadedFiles[0].fd.split('/');
+            var tam = lista.length;
+            var nomeFoto = lista[tam-1];
+            Usuario.query(`INSERT INTO c9.Usuario (CPF,Nome, Nascimento,RG,Email,Senha,Endereco,Telefone,Cidade,Estado,Sexo, Foto) VALUES("${req.body.CPF}","${req.body.Nome}","${req.body.Nascimento}","${req.body.RG}","${req.body.Email}","${req.body.Senha}","${req.body.Endereco}","${req.body.Telefone}","${req.body.Cidade}","${req.body.Estado}","${req.body.Sexo}", "${nomeFoto}");`, [], function(err, usuarios){
+                if(err){
+                  res.send(500, {error: "Database error"});
+                }
+                res.redirect('/usuario/list');
+              });
+    	});
   },
   about: function(req,res){
     Usuario.query(`SELECT * from c9.Usuario WHERE CPF="${req.params.id}";`, ['u'], function(err, usuario) {
@@ -38,7 +48,7 @@ module.exports = {
     })
   },
   edit: function(req,res){
-    Usuario.query(`SELECT * from banco.usuario WHERE cpf="${req.params.id}";`, [], function(err, usuario){
+    Usuario.query(`SELECT * from c9.Usuario WHERE cpf="${req.params.id}";`, [], function(err, usuario){
       if(err){
         res.send(500, {error: "Database error"});
       }
@@ -47,13 +57,37 @@ module.exports = {
     });
   },
   update: function(req,res){
-    console.log(`${req.body.nascimento}`);
-    Usuario.query(`UPDATE banco.usuario SET nome="${req.body.nome}", nascimento="${req.body.nascimento}", rg="${req.body.rg}", email="${req.body.email}", senha="${req.body.senha}", endereco="${req.body.endereco}", telefone="${req.body.telefone}", cidade="${req.body.cidade}", estado="${req.body.estado}" WHERE cpf="${req.body.cpf}";`, [], function(err, p){
-      if(err){
-        res.send(500, {error: "Database error"});
-      }
-      res.redirect('/usuario/list');
-    });
+    console.log(req.body);
+    
+    /* Quer mudar a foto */
+		if (req.body.FotoPerfil != '') {
+		  req.file('FotoPerfil').upload({ 
+    		dirname: require('path').resolve(sails.config.appPath, 'assets/images/usuario'),
+    		saveAs: req.body.CPF + '_' + "FotoPerfil." + (req.file('FotoPerfil')._files[0].stream.filename).substr(req.file('FotoPerfil')._files[0].stream.filename.length - 3)
+	    	}, function (err, uploadedFiles) {
+			  
+			  console.log(uploadedFiles);
+				var lista = uploadedFiles[0].fd.split('/');
+        var tam = lista.length;
+        var nomeFoto = lista[tam-1];
+        
+		    Usuario.query(`UPDATE c9.Usuario SET Nome="${req.body.Nome}", Nascimento="${req.body.Nascimento}", RG="${req.body.RG}", Email="${req.body.Email}", Senha="${req.body.Senha}", Endereco="${req.body.Endereco}", Telefone="${req.body.Telefone}",  Cidade="${req.body.Cidade}", Estado="${req.body.Estado}", Foto="${nomeFoto}" WHERE CPF="${req.body.CPF}";`, [], function(err, p){
+          if(err){
+            res.send(500, {error: "Database error"});
+          }
+          res.redirect('/usuario/list');
+        });
+		  })
+		}
+		else{
+		  Usuario.query(`UPDATE c9.Usuario SET Nome="${req.body.Nome}", Nascimento="${req.body.Nascimento}", RG="${req.body.RG}", Email="${req.body.Email}", Senha="${req.body.Senha}", Endereco="${req.body.Endereco}", Telefone="${req.body.Telefone}",  Cidade="${req.body.Cidade}", Estado="${req.body.Estado}" WHERE CPF="${req.body.CPF}";`, [], function(err, p){
+          if(err){
+            res.send(500, {error: "Database error"});
+          }
+          res.redirect('/usuario/list');
+      });
+		}
+    
   },
   delete:function(req,res){
     Usuario.query(`DELETE FROM banco.usuario WHERE cpf="${req.params.id}";`, [], function(err){
