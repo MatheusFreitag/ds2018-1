4c4528aceab838f9d53bea5201d9cc1d6db74220
Renata Junges <renatajunges@hotmail.com>
2018-07-26 15:48:46 +0000

    Correcao do datapicker

--- assets/js/datepicker.js
+++ assets/js/datepicker.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	var date_input = $('input[class="form-control date"]'); //our date input has the name "date"
-	var container = $('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+	//var container = $('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
 	date_input.datepicker({
 		format: 'dd/mm/yyyy',
 		language: "pt-BR"
